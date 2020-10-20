0c6396be2698c003204fab108eb91d081a600f89
Kieiru <55286594+Kieiru@users.noreply.github.com>
2020-10-20 20:23:44 +0200

    Update app2.js

--- app2.js
+++ app2.js
@@ -83,7 +83,7 @@ function newGame(){
     document.querySelector('#score-0').textContent = 0;
     document.querySelector('#score-1').textContent = 0;
     document.querySelector('#current-0').textContent = 0;
-    document.querySelector('#current=1').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
     activePlayer = 0;
 }
 
